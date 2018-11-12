65171bbac4319bc191d7f286d9da658c12678d34
weipi <332107301>
2018-11-12 09:39:22 +0800

    删除无用类TreeView

---
+++ src/main/java/com/wpr/treeviewcore/model/TreeViewItem.java
@@ -0,0 +1,68 @@
+package com.wpr.treeviewcore.model;
+
+
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by 丕汝 on 2018/10/22.
+ */
+
+public class TreeViewItem {
+    private ImageView expandIconView;
+    private ImageView nodeIconView;
+    private TextView nodeNameView;
+    private CheckBox checkBox;
+    private TextView nodeIDView;
+    private boolean isExpand;
+
+    public ImageView getExpandIconView() {
+        return expandIconView;
+    }
+
+    public void setExpandIconView(ImageView expandIconView) {
+        this.expandIconView = expandIconView;
+    }
+
+    public ImageView getNodeIconView() {
+        return nodeIconView;
+    }
+
+    public void setNodeIconView(ImageView nodeIconView) {
+        this.nodeIconView = nodeIconView;
+    }
+
+    public CheckBox getCheckBox() {
+        return checkBox;
+    }
+
+    public void setCheckBox(CheckBox checkBox) {
+        this.checkBox = checkBox;
+    }
+
+    public TextView getNodeNameView() {
+        return nodeNameView;
+    }
+
+    public void setNodeNameView(TextView nodeNameView) {
+        this.nodeNameView = nodeNameView;
+    }
+
+    public TextView getNodeIDView() {
+        return nodeIDView;
+    }
+
+    public void setNodeIDView(TextView nodeIDView) {
+        this.nodeIDView = nodeIDView;
+    }
+
+
+    public boolean isExpand() {
+        return isExpand;
+    }
+
+    public void setExpand(boolean expand) {
+        isExpand = expand;
+    }
+}
